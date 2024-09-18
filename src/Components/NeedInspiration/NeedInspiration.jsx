228a5360797ce9368df6613cc56fb6290f204683
VannelopeXD <sofia.gozzoli@hotmai.com>
2024-09-18 00:37:26 -0300

    att 18/09

--- src/Components/NeedInspiration/NeedInspiration.jsx
+++ src/Components/NeedInspiration/NeedInspiration.jsx
@@ -3,23 +3,23 @@ import './NeedInspiration.css';
 import { Container } from "react-bootstrap";
 
 function NeedInspiration() {
-    return(
-    <div className="need-inspiration py-5 position-relative">
-        <div className='bg-shape position-absolute'></div>
-        <Container>
-            <div className="row">
-                <div className='col-md-4 z-2'>
-                    <h2 className='text-start fw-semibold'>Precisando de ajuda para montar o roteiro?</h2>
-                    <p className='text-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore soluta animi qui, consequatur expedita.</p>
-                    <div className='d-flex'>
-                        <h5 className='fw-semibold text-nowrap'>Contato:</h5>
-                        <a href="/" className='mx-2 text-decoration-none h5 fw-semibold'>0900800700</a>
+    return (
+        <div className='need-inspiration py-5 position-relative'>
+            <div className='bg-shape position-absolute'></div>
+            <Container>
+                <div className="row">
+                    <div className='col-md-4 z-2'>
+                        <h2 className='text-start text-light text-capitalize fw-semibold'>Need inspiration?</h2>
+                        <p className='text-start text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore soluta animi qui, consequatur expedita.</p>
+                        <div className='d-flex'>
+                            <h5 className='text-light text-capitalize fw-semibold text-nowrap'>Call us:</h5>
+                            <a href="/" className='text-light mx-2 text-decoration-none h5 fw-semibold'>0900800700</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Container>
-    </div>
-  )
-}
-
-export default NeedInspiration;
+            </Container>
+        </div>
+      )
+    }
+    
+    export default NeedInspiration;
